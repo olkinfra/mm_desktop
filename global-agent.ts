4daaa53005154b351fd235037e0ed9461653e4a0
olkinfra <140081516+olkinfra@users.noreply.github.com>
2023-07-28 11:19:47 +0200

    Update global-agent.ts

--- global-agent.ts
+++ global-agent.ts
@@ -1,11 +1,3 @@
-import { getEnv, setEnv } from './utils';
-
-const env = getEnv('GLOBAL_AGENT_');
-
-setEnv('GLOBAL_AGENT_HTTP_PROXY', env('HTTP_PROXY'));
-setEnv('GLOBAL_AGENT_HTTPS_PROXY', env('HTTPS_PROXY'));
-setEnv('GLOBAL_AGENT_NO_PROXY', env('NO_PROXY'));
-
 require('global-agent').bootstrap();
  
 
